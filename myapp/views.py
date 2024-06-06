1c39d8fc3db305e5939b8ba0ddf911a8edfa6cd0
kflorkowski <k.florkowski98@gmail.com>
2024-06-07 01:32:07 +0200

    2.04.01-people-view-creation

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import PersonForm
 
 
 @login_required
 def home(request):
     return render(request, 'home.html')
+
+
+def people(request):
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            form.save(user=request.user)
+            return redirect('people')
+    else:
+        form = PersonForm()
+    return render(request, 'people.html', {'form': form})
