cdded53157e8ad36e27e44390d1c7490fcd86c07
kflorkowski <k.florkowski98@gmail.com>
2024-06-07 01:19:40 +0200

    2.02.01-person-model-creation

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Person(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    firstname = models.CharField(max_length=128)
+    lastname = models.CharField(max_length=128)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    birth_date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
