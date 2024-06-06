71834b194191d7262053ab5dab870b6ecf230f26
kflorkowski <k.florkowski98@gmail.com>
2024-06-06 14:02:08 +0200

    1.05.00-users/urls.py-create

--- finance_helper/urls.py
+++ finance_helper/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from users.views import register
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', register, name='register')
+    path('', include('users.urls')),
 ]
