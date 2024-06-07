61875937a4dccb16d9593316e45c75f05448bbe5
kflorkowski <k.florkowski98@gmail.com>
2024-06-07 04:53:02 +0200

    job-added

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('people/', views.people, name='people'),
     path('people/<int:person_id>/', views.personal_details, name='personal_details'),
+    path('people/<int:person_id>/job_list', views.job_list, name='job_list'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .forms import PersonForm
-from .models import Person
+from .forms import PersonForm, JobForm
+from .models import Person, Job
 
 
 @login_required
@@ -27,3 +27,20 @@ def personal_details(request, person_id):
     #     return render(request, 'unauthorized.html')
     jobs = person.jobs.all()
     return render(request, 'personal_details.html', {'person': person, 'jobs': jobs})
+
+
+def job_list(request, person_id):
+    person = get_object_or_404(Person, id=person_id, user=request.user)
+    jobs = person.jobs.all()
+
+    if request.method == 'POST':
+        form = JobForm(request.POST)
+        if form.is_valid():
+            job = form.save(commit=False)
+            job.person = person
+            job.save()
+            return redirect('job_list', person_id=person.id)
+    else:
+        form = JobForm()
+
+    return render(request, 'job_list.html', {'form': form, 'jobs': jobs, 'person': person})
